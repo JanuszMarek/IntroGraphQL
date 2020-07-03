1246298881167332cdd6400a94b980af16c6813e
Marek <janusz.marek@corp.altegrity.com>
2020-07-03 14:53:26 +0200

    Repo

---
+++ IntroGraphQL/Repositories/BooksRepository.cs
@@ -0,0 +1,21 @@
+﻿using IntroGraphQL.Database;
+using IntroGraphQL.Entities;
+using System.Collections.Generic;
+
+namespace IntroGraphQL.Repositories
+{
+    public class BooksRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public BooksRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IEnumerable<Book> GetAll()
+        {
+            return dbContext.Books;
+        }
+    }
+}
