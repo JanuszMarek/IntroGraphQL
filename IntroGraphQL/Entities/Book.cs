e8d215e8a7614704470a48a681f01802e68e8065
Marek <janusz.marek@corp.altegrity.com>
2020-07-03 14:45:53 +0200

    Entities

---
+++ IntroGraphQL/Entities/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntroGraphQL.Entities
+{
+    public class Book
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [MaxLength(250)]
+        public string Title { get; set; }
+
+        [ForeignKey(nameof(Author))]
+        public long AuthorId { get; set; }
+        public Author Author { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        [MaxLength(2000)]
+        public string Description { get; set; }
+    }
+}
